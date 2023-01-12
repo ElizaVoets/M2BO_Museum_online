4ffa4246cbc811fb8c3c0b448223d95e6318750d
Eliza Voets <25413@ma-web.nl>
2023-01-12 14:56:04 +0100

    redo

--- codes/main.js
+++ codes/main.js
@@ -1,12 +1,4 @@
 function show(){
     document.querySelector('.hamburger').classList.toggle('open')
-    document.querySelector('.navigation').classList.toggle('active')
+    document.querySelector('nav').classList.toggle('active')
 }
-
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('ul li a').forEach(link => {
-    if(link.href.includes(`${activePage}`)){
-        link.classList.add('capage');
-    }
-})
-
